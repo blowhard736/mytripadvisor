129430fd638d3d5e518889a626a6ab582e35c3d5
ddabae27 <ddabae27@gmail.com>
2018-09-18 09:43:00 +0900

    만제 register.js 수정

--- WebContent/scripts/register.js
+++ WebContent/scripts/register.js
@@ -64,7 +64,10 @@ function validateEmail(email) {
 	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	return re.test(email);
 }
-
+function validatePassword(password){
+	var re = /^(?=.{8,})(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$/;
+	return re.test(password);
+}
 function submit(email, password, gender, birth) {
 	var params = {
 		email : email,
